7709332d2512e4f6119e1bdfcc2138dfa53b2649
pijushdas <pijushdas@gmail.com>
2021-07-13 00:29:15 +0530

    Update SQL files.

--- src/main/java/com/natwest/NatwestTransactionApi.java
+++ src/main/java/com/natwest/NatwestTransactionApi.java
@@ -2,13 +2,14 @@ package com.natwest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * Use this class to load the Spring boot context.
  * @author Pijush Kanti Das
  *
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NatwestTransactionApi {
 
 	public static void main(String[] args) {
--- src/main/java/com/natwest/integration/entity/transactiondomain/TransactionEntity.java
+++ src/main/java/com/natwest/integration/entity/transactiondomain/TransactionEntity.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -18,12 +19,12 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "TRANSACTION")
+@SequenceGenerator(name="TRANSACTION_SEQUENCE", initialValue=1, allocationSize=1)
 public class TransactionEntity {
 
 	@Id
 	@Column(name = "TRANSACTION_ID")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="TRANSACTION_SEQUENCE")
 	private Integer transactionId;
 	
 	@Column(name = "SOURCE_ACCOUNT_NUMBER")
